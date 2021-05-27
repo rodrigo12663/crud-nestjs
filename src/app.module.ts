7aec02a1cc0b9ae225d8bb03afb5a952521d8edc
Rodrigo <rodrigovieiranunes23@outlook.com>
2021-05-27 18:35:41 -0300

    create environment variable para docker-composer and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
